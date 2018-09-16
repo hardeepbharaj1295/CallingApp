da492420bf5b2fb4ac3b8050bde2d3246fb4f84b
Hardeep Singh <hardeepbharaj1295@gmail.com>
2018-09-16 22:50:29 +0530

    Get id of EditText and Button. Action Click listener performed on button

--- app/src/main/java/com/o7services/callingapp/CallingActivity.java
+++ app/src/main/java/com/o7services/callingapp/CallingActivity.java
@@ -2,14 +2,32 @@ package com.o7services.callingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class CallingActivity extends AppCompatActivity {
+
+    EditText editText;
+    ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calling);
 
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.imageButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CallingActivity.this, "Calling", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
+
+
 }
