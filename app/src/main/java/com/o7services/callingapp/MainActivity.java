662e488357e26f28dd78055a2063d592b091b0e7
Hardeep Singh <hardeepbharaj1295@gmail.com>
2018-09-16 22:38:40 +0530

    Hide Toolbar Jump MainActivity to CallingActivity

--- app/src/main/java/com/o7services/callingapp/MainActivity.java
+++ app/src/main/java/com/o7services/callingapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(MainActivity.this,CallingActivity.class));
+                    finish();
+                }
+            }
+        }).start();
     }
 }
