28055d16af572dd4806c213869a52b55f60768e8
Hardeep Singh <hardeepbharaj1295@gmail.com>
2018-09-16 22:32:03 +0530

    Initial commit

---
+++ app/src/main/java/com/o7services/callingapp/CallingActivity.java
@@ -0,0 +1,13 @@
+package com.o7services.callingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calling);
+    }
+}
